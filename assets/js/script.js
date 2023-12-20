fd57702325727f1296666853fff185b9fe25d986
Nuket <nukumoonday@gmail.com>
2023-12-20 13:17:54 +0000

    I have added current day and color variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,105 +1,94 @@
 
+var currentTime = dayjs().hour();
+var time = 0;
+console.log(currentTime);
+
 function displayDate() {
+  var currentDay = dayjs().format('dddd, MMMM, D, YYYY');
+  $('#currentDay').text(currentDay);
+}
 
-    var currentDay = dayjs().format('dddd, MMMM, D, YYYY');
-    $('#currentDay').text(currentDay);
-  }
-  displayDate(); 
-  
+function setBackground() {
 
-  var currentTime = dayjs().hour();
-  var time = 0;
-
-  if (currentTime<9){
-    $('textarea.col-md-10').addClass('future');
-    $('textarea.col-md-10').addClass('bg-success');
-  }else if (currentTime === 9 ){
-    $('textarea.col-md-10').addClass('present');
-    $('textarea.col-md-10').addClass('present');
-  }else if (currentTime > 9 ){
-    $('textarea.col-md-10').addClass('past');
-    $('textarea.col-md-10').addClass('bg-secondary-subtle');
-  }
-  if (currentTime<10){
-    $('textarea.col-md-10').addClass('future');
-    $('textarea.col-md-10').addClass('bg-success');
-  }else if (currentTime === 10 ){
-    $('textarea.col-md-10').addClass('present');
-    $('textarea.col-md-10').addClass('present');
-  }else if (currentTime.hour() > 10 ){
-    $('textarea.col-md-10').addClass('past');
-    $('textarea.col-md-10').addClass('bg-secondary-subtle');
-  }
-  if (currentTime <11){
-    $('textarea.col-md-10').addClass('future');
-    $('textarea.col-md-10').addClass('bg-success');
-  }else if (currentTime === 11 ){
-    $('textarea.col-md-10').addClass('present');
-    $('textarea.col-md-10').addClass('bg-danger');
-  }else if (currentTime > 11 ){
-    $('textarea.col-md-10').addClass('past');
-    $('textarea.col-md-10').addClass('bg-secondary-subtle');
-  }
-  if (currentTime <12){
-    $('textarea.col-md-10').addClass('future');
-    $('textarea.col-md-10').addClass('bg-success');
-  }else if (currentTime === 12 ){
-    $('textarea.col-md-10').addClass('present');
-    $('textarea.col-md-10').addClass('bg-danger');
-  }else if (currentTime > 12 ){
-    $('textarea.col-md-10').addClass('past');
-    $('textarea.col-md-10').addClass('bg-secondary-subtle');
-  }
-  if (currentTime <13){
-    $('textarea.col-md-10').addClass('future');
-    $('textarea.col-md-10').addClass('bg-success');
-  }else if (currentTime  === 13 ){
-    $('textarea.col-md-10').addClass('present');
-    $('textarea.col-md-10').addClass('bg-danger');
-  }else if (currentTime > 13 ){
-    $('textarea.col-md-10').addClass('past');
-    $('textarea.col-md-10').addClass('bg-secondary-subtle');
-  }
-  if (currentTime <14){
-    $('textarea.col-md-10').addClass('future');
-    $('textarea.col-md-10').addClass('bg-success');
-  }else if (currentTime === 14 ){
-    $('textarea.col-md-10').addClass('present');
-    $('textarea.col-md-10').addClass('bg-danger');
-  }else if (currentTime > 14 ){
-    $('textarea.col-md-10').addClass('past');
-    $('textarea.col-md-10').addClass('bg-secondary-subtle');
-  }
-  if (currentTime <15){
-    $('textarea.col-md-10').addClass('future');
-    $('textarea.col-md-10').addClass('bg-success');
-  }else if (currentTime === 15 ){
-    $('textarea.col-md-10').addClass('present');
-    $('textarea.col-md-10').addClass('bg-danger');
-  }else if (currentTime > 15 ){
-    $('textarea.col-md-10').addClass('past');
-    $('textarea.col-md-10').addClass('bg-secondary-subtle');
-  }
-  if (currentTime <16){
-    $('textarea.col-md-10').addClass('future');
-    $('textarea.col-md-10').addClass('bg-success');
-  }else if (currentTime === 16 ){
-    $('textarea.col-md-10').addClass('present');
-    $('textarea.col-md-10').addClass('bg-danger');
-  }else if (currentTime > 16 ){
-    $('textarea.col-md-10').addClass('past');
-    $('textarea.col-md-10').addClass('bg-secondary-subtle');
-  }
-  if (currentTime<17){
-    $('textarea.col-md-10').addClass('future');
-    $('textarea.col-md-10').addClass('bg-success');
-  }else if (currentTime === 17 ){
-    $('textarea.col-md-10').addClass('present');
-    $('textarea.col-md-10').addClass('bg-danger');
-  }else if (currentTime > 17 ){
-    $('textarea.col-md-10').addClass('past');
-    $('textarea.col-md-10').addClass('bg-secondary-subtle');
+  if (currentTime < 9) {
+    $('#text09').addClass('future');
+  } else if (currentTime === 9) {
+    $('#text09').addClass('present');
+  } else if (currentTime > 9) {
+    $('#text09').addClass('past');
   }
 
- 
+  if (currentTime < 10) {
+    $('#text10').addClass('future');
+  } else if (currentTime === 10) {
+    $('#text10').addClass('present');
+  } else if (currentTime > 10) {
+    $('#text10').addClass('past');
+  }
 
+  if (currentTime < 11) {
+    $('#text11').addClass('future');
+  } else if (currentTime === 11) {
+    $('#text11').addClass('present');
+  } else if (currentTime > 11) {
+    $('#text11').addClass('past');
+  }
+
+  if (currentTime < 12) {
+    $('#text12').addClass('future');
+  } else if (currentTime === 12) {
+    $('#text12').addClass('present');
+  } else if (currentTime > 12) {
+    $('#text12').addClass('past');
+  }
+
+  if (currentTime < 13) {
+    $('#text13').addClass('future');
+  } else if (currentTime === 13) {
+    $('#text13').addClass('present');
+  } else if (currentTime > 13) {
+    $('#text13').addClass('past');
+  }
+
+  if (currentTime < 14) {
+    $('#text14').addClass('future');
+  } else if (currentTime === 14) {
+    $('#text14').addClass('present');
+  } else if (currentTime > 14) {
+    $('#text14').addClass('past');
+  }
+
+  if (currentTime < 15) {
+    $('#text15').addClass('future');
+  } else if (currentTime === 15) {
+    $('#text15').addClass('present');
+  } else if (currentTime > 15) {
+    $('#text15').addClass('past');
+  }
+
+  if (currentTime < 16) {
+    $('#text16').addClass('future');
+  } else if (currentTime === 16) {
+    $('#text16').addClass('present');
+  } else if (currentTime > 16) {
+    $('#text16').addClass('past');
+  }
+
+  if (currentTime < 17) {
+    $('#text17').addClass('future');
+  } else if (currentTime === 17) {
+    $('#text17').addClass('present');
+  } else if (currentTime > 17) {
+    $('#text17').addClass('past');
+  }
+}
+//click event
+$('.saveBtn').on('click', function(){
+  var keyVal = $(this).prev().val();
+  var keyName = $(this).prev().attr('id');
+ localStorage.setItem(keyName, keyVal)
+})
+
+displayDate();
+
+setBackground()
